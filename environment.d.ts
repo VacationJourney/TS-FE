0290735cb5aaeb42057ace22ba0c6ee02ef038bd
Jeremy McWilliams <j2macwilliams@gmail.com>
2021-03-19 10:27:49 -0700

    change .env var for BE back

--- environment.d.ts
+++ environment.d.ts
@@ -2,7 +2,7 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     REACT_APP_AUTH0_DOMAIN: string,
     REACT_APP_AUTH0_CLIENT_ID: string,
-    REACT_APP_SERVER_URL: string,
+    REACT_APP_ENDPOINT: string,
     REACT_APP_AUTH0_AUDIENCE: string
   }
 }
