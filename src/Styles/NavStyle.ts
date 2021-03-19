4485e7596fd5b9c48dfede8f556a165e5d0e7762
Jeremy McWilliams <j2macwilliams@gmail.com>
2021-03-19 09:17:00 -0700

    re-work Navbar for TS and fix styles

---
+++ src/Styles/NavStyle.ts
@@ -0,0 +1,34 @@
+import { makeStyles } from '@material-ui/core';
+
+export const useStyles = makeStyles((theme) => ({
+  navbar: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: '1%',
+    padding: 5,
+  },
+  logoAnchor: {
+    width: '10%'
+  },
+  journeyCostLogo: {
+    width: '100%',
+    transition: '.03s ease-in-out',
+    '&:hover': {
+      transform: 'scale(1.1)',
+    }
+  },
+  userImage: {
+    width: 40,
+    borderRadius: '50%',
+    marginTop: 3,
+  },
+  menu: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  menuItem: {
+    transition: '.03s ease-in-out',
+    margin: '0 5px',
+  },
+}))
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Link, useRouteMatch, useHistory } from 'react-router-dom'
+import { useAuth0 } from "@auth0/auth0-react";
+import Menu from './Menu'
+import HomeIcon from '@material-ui/icons/Home';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import { Hidden } from '@material-ui/core'
+import { useStyles } from '../../Styles/NavStyle'
+import JCost from '../../assets/J.png'
+
+type NavProps = {
+  picture: string,
+  client: any
+}
+const Navbar = ({ picture, client }: NavProps) => {
+  const classes = useStyles()
+  let { url } = useRouteMatch();
+  const history = useHistory();
+  const { logout } = useAuth0();
+
+  const exit =  () => {
+    client.resetStore();
+    logout({
+      returnTo: window.location.origin,
+    });
+  };
+
+  return (
+    <div className={classes.navbar}>
+      <Link className={classes.logoAnchor} to={`${url}`}>
+        <img src={JCost} className={classes.journeyCostLogo} alt="Journey Co$t Logo" />
+      </Link>
+      <nav className={classes.menu}>
+        <img
+          src={picture}
+          alt="Profile"
+          className={classes.userImage}
+
+        />
+        <Hidden smDown>
+          <HomeIcon className={classes.menuItem} onClick={() => history.push('/vacations')} fontSize='large'></HomeIcon>
+          <ExitToAppIcon className={classes.menuItem} onClick={exit} fontSize='large' />
+        </Hidden>
+        <Hidden mdUp>
+          <Menu exit={exit} />
+        </Hidden>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
